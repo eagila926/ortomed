64e9b8a6f10349bfe1d5ddd8efdedd74c64292af
Elvis Agila <elvis.agila@ESCOLLANOS.LOCAL>
2024-08-19 16:54:00 -0500

    Formulario formulas no establecidas

--- formulas-no-establecidas.php
+++ formulas-no-establecidas.php
@@ -38,13 +38,78 @@ if (!isset($_SESSION['user_id'])) {
 
                 <!-- Start Content-->
                 <div class="container-fluid">
+                    <div class="row" style="margin-top: 10px;">
+                            <div class="col-sm-12">
+                                <div class="card card-body">
+                                    <h3 class="card-title">Ingrese los activos</h3>
+                                    <div class="row">
+                                        <div class="col-lg-12">
+                                            <form>
+                                                <div class="mb-12">
+                                                    <label for="simpleinput" class="form-label">Activo:</label>
+                                                    <input type="text" class="form-control" id="formula" name="formula"
+                                                    placeholder="Ingrese el nombre del activo" autocomplete="off">
+                                                </div>
+                                            
 
-                    
+                                        </div>
+                                        <div class="col-lg-6">
+                                            <div class="mb-6">
+                                                    <label for="example-number" class="form-label">Cantidad:</label>
+                                                    <input type="number" class="form-control" id="cant" name="cant" required>
+                                                </div>
 
-                            </div> <!-- end card-body-->
-                        </div> <!-- end card-->
-                    </div> <!-- end col-->
-                </div> <!-- end row -->
+                                        </div>
+                                        <div class="col-lg-6">
+                                            <div class="mb-3">
+                                                <label for="unidad" class="form-label">Unidad:</label>
+                                                <select class="form-select" id="example-select" name="unidad">
+                                                    <option value=""></option>
+                                                    <option value="g">g</option>
+                                                    <option value="mg">mg</option>
+                                                    <option value="mcg">mcg</option>
+                                                    <option value="UI">UI</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        
+                                    </form>
+                                        
+                                    </div>
+                                    
+
+                                </div> <!-- end card-->
+                            </div> <!-- end col-->
+                        </div>  
+
+                        <div class="row" style="margin-top: 10px;">
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h4 class="header-title">Activos Seleccionados</h4>
+                                    <table id="state-saving-datatable" class="table activate-select dt-responsive nowrap w-100">
+                                        <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>Activo</th>
+                                                <th>Cantidad</th>
+                                                <th>Unidad</th>
+                                                <th>Eliminar</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                        </tbody>
+                                    </table>
+
+                                </div> <!-- end card body-->
+                            </div> <!-- end card -->
+                        </div><!-- end col-->
+                    </div> <!-- end row-->                             
+
+                                </div> <!-- end card-->
+                            </div> <!-- end col-->
+                        </div>  
+                
                 </div>
                 <!-- container -->
 
